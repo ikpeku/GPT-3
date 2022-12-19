9e695f6cf67ef2a425cce96c05013e04fbeecf04
ikpeku <ikpedaniel07@gmail.com>
2022-12-20 00:44:38 +0100

    feat: home, login, signup pages created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import { Navbar, NotFound } from "./component";
+import { Login, Signup } from "./pages";
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/" element={<Navbar/>}>
+        <Route path="login" element={<Login/>} />
+        <Route path="signup" element={<Signup/>} />
+
+        </Route>
+        <Route path="*" element={<NotFound/>} />
+      </Routes>
    
     </div>
   );
